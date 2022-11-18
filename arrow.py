0d7ba018ee22a9f921cd9f5575330ccf77354a4c
khludnegm <116512645+khludnegm@users.noreply.github.com>
2022-11-19 01:42:16 +0200

    Add files via upload

---
+++ arrow.py
@@ -0,0 +1,174 @@
+import os
+from time import sleep
+cntr=0
+y=range(1,17,1)
+for n in (y):
+			if(cntr==0 or cntr<5):	
+			        print("                *")
+			        cntr +=1
+			if(cntr==5): 
+					print("               ***")
+					cntr +=1
+			if(cntr==6): 
+					print("              *****")
+					cntr +=1
+			if(cntr==7): 
+					print("             *******")
+					cntr +=1
+			if(cntr==8): 
+					print("            *********")
+					cntr +=1
+			if(cntr==9): 
+					print("           ***********")
+					cntr +=1
+			if(cntr==10): 
+					print("          *************")
+					cntr +=1	
+x=range(1,8,1)
+for n in (x):
+			print("                *")
+			         					
+'''first stars up'''
+
+
+# Clearing the Screen
+sleep(0.2)
+os.system('cls')
+
+'''right'''
+cntr3=0
+x=range(25)
+for n in (x):
+			if(cntr3==0 or cntr3<=11):	
+			        print("")
+			        cntr3 +=1
+			elif(cntr3==12): 
+					print("                       *")
+					cntr3 +=1
+			elif(cntr3==13): 
+					print("                       **")
+					cntr3 +=1
+			elif(cntr3==14): 
+					print("                       ***")
+					cntr3 +=1
+			elif(cntr3==15): 
+					print("                       ****")
+					cntr3 +=1
+			elif(cntr3==16): 
+					print("                       *****")
+					cntr3 +=1
+			elif(cntr3==17): 
+					print("                       ******")
+					cntr3 +=1	
+			elif(cntr3==18): 
+					print("                ******************")
+					cntr3 +=1
+			elif(cntr3==19): 
+					print("                       ******")
+					cntr3 +=1	
+			elif(cntr3==20): 
+					print("                       *****")
+					cntr3 +=1
+			elif(cntr3==21): 
+					print("                       ****")
+					cntr3 +=1	
+			elif(cntr3==22): 
+					print("                       ***")
+					cntr3 +=1		
+			elif(cntr3==23): 
+					print("                       **")
+					cntr3 +=1			
+					
+			elif(cntr3==24): 
+					print("                       *")
+					cntr3 +=1
+					break;
+sleep(0.2)
+os.system('cls')
+'''down'''
+x=range(1,8,1)
+for n in (x):
+			print("                *")
+cntr4=0
+c=range(1,17,1)
+for n in (c):
+			if(cntr4==0 ):
+						cntr4 +=1
+						print("          *************")
+						
+			elif(cntr4==1): 
+						print("           ***********")
+						cntr4 +=1
+			elif(cntr4==2): 
+						print("            *********")
+						cntr4 +=1
+			elif(cntr4==3): 
+					print("             *******")
+					cntr4 +=1
+			elif(cntr4==4): 
+					print("              *****")
+					cntr4 +=1
+			elif(cntr4==5): 
+					print("               ***")
+					cntr4 +=1
+			elif(cntr4==6): 
+					print("                *")
+					cntr4 +=1	
+					break;
+
+x=range(1,5,1)
+for n in (x):
+			print("                *")
+
+'''left '''
+sleep(0.2)
+os.system('cls')
+cntr2=0
+x=range(25)
+for n in (x):
+			if(cntr2==0 or cntr2<=11):	
+			        print("")
+			        cntr2 +=1
+			elif(cntr2==12): 
+					print("         *")
+					cntr2 +=1
+			elif(cntr2==13): 
+					print("        **")
+					cntr2 +=1
+			elif(cntr2==14): 
+					print("       ***")
+					cntr2 +=1
+			elif(cntr2==15): 
+					print("      ****")
+					cntr2 +=1
+			elif(cntr2==16): 
+					print("     *****")
+					cntr2 +=1
+			elif(cntr2==17): 
+					print("    ******")
+					cntr2 +=1	
+			elif(cntr2==18): 
+					print("******************")
+					cntr2 +=1
+			elif(cntr2==19): 
+					print("    ******")
+					cntr2 +=1	
+			elif(cntr2==20): 
+					print("     *****")
+					cntr2 +=1
+			elif(cntr2==21): 
+					print("      ****")
+					cntr2 +=1	
+			elif(cntr2==22): 
+					print("       ***")
+					cntr2 +=1		
+			elif(cntr2==23): 
+					print("        **")
+					cntr2 +=1			
+					
+			elif(cntr2==24): 
+					print("         *")
+					cntr2 +=1	
+					break;
+		
+						
